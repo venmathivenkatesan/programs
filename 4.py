ccc1ad46a78ceab884a66d1af8e9d707c63b190f
venmathivenkatesan <52342535+venmathivenkatesan@users.noreply.github.com>
2019-06-29 22:28:48 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,5 +1,4 @@
-z=input("")
-if z=='a'or z=='b'or z=='c'or z=='d'or z=='e'or z=='f'or z=='g'or z=='h'or z=='i'or z=='j'or z=='k'or z=='l'or z=='m'or z=='n'or z=='o'or z=='p'or z=='q'or z=='r'or z=='s'or z=='t'or z=='u'or z=='v'or z=='w'or z=='x'or z=='y'or z=='z'or z=='A'or z=='B'or z=='C'or z=='D'or z=='E'or z=='F'or z=='G'or z=='H'or z=='I'or z=='J'or z=='K'or z=='L'or z=='M'or z=='N'or z=='O'or z=='P'or z=='Q'or z=='R'or z=='S'or z=='T'or z=='U'or z=='V'or z=='W'or z=='X'or z=='Y'or z=='Z':
-    print("Alphabet")
+na1=input()
+if(na1 >='a' and na1<='z') or (na1>='A' and na1<='Z'):
 else:
-      print("no")
+    print("No")
