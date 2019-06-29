f897dec651709e3d79fd737331a1a69a6a1b8720
venmathivenkatesan <52342535+venmathivenkatesan@users.noreply.github.com>
2019-06-29 22:35:41 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,5 +1,5 @@
-na1=input()
-if(na1 >='a' and na1<='z') or (na1>='A' and na1<='Z'):
+va1=input()
+if(va1 >='a' and va1<='z') or (va1>='A' and va1<='Z'):
     print("Alphabet")
 else:
     print("No")
